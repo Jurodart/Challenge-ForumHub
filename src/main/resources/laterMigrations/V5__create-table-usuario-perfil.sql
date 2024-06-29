84c63349a01b79f880980d3692dc57a35dbd860b
Juliana Rodrigues <julianarodriguesart@gmail.com>
2024-06-28 21:32:24 -0300

    codigos basicos

--- src/main/resources/laterMigrations/V5__create-table-usuario-perfil.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE usuario_perfil (
-    usuario_id BIGINT,
-    perfil_id BIGINT,
-    PRIMARY KEY (usuario_id, perfil_id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (perfil_id) REFERENCES perfis(id)
-);
